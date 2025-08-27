11b906a8bc132bb17e97a359e30f849b0b38f263
Daniel Guzmán <135665168+DanielGuzman21@users.noreply.github.com>
2025-08-26 20:58:05 -0500

    Update PUNTO_6.py

--- PUNTO_6.py
+++ PUNTO_6.py
@@ -7,11 +7,12 @@ d_cilindro = 300 # mm
 l_cilindro = 90  # mm
 
 #fuerza de avance
-fuerza_avance = abs((p_entrada - p_salida) * (math.pi * (d_cilindro/2)**2))
+fuerza_avance = abs(p_entrada * (math.pi * (d_cilindro/2)**2))
 print("Fuerza de avance:", fuerza_avance, "N")
 
 #fuerza de retroceso
 fuerza_retroceso = abs(p_salida * (math.pi * (d_cilindro/2)**2) - math.pi *(d_cilindro/2)**2)
 print("Fuerza de retroceso:", fuerza_retroceso, "N")
+
 
 
